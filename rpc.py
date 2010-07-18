b89ff92137653c9cf133a6de6b2166c5e88d12ad
Isaac Hawley <isaachawley@gmail.com>
2010-07-18 16:42:20 +0800

    fixing listing, rpc

--- rpc.py
+++ rpc.py
@@ -15,45 +15,46 @@ import models
 import geocode
 
 class MainHandler(webapp.RequestHandler):
-	def getNearby(self):
-		lat = self.request.get("lon")
-		lon = self.request.get("lat")
-		mainHash = {} #main response hash
-		mapHash = {} #map data hash
-		posts = models.Post.proximity_fetch(
-					models.Post.all(), 
-					db.GeoPt(float(lon), float(lat)), 
-					max_results=10)
-		temps = []
-		for p in posts:
-			plocs = {} #dict for posts
-			plocs["lon"] = p.location.lat
-			plocs["lat"] = p.location.lon
-			plocs["id"] = p.key().id().__str__()
-			plocs["picid"] = p.pic.key().id().__str__()
-			plocs["content"] = p.content
-			mapHash[p.key().__str__()] = plocs
-			temps.append(plocs)
-			
-		mainHash["mapdata"] = mapHash
+  def getNearby(self):
+    lat = self.request.get("lon")
+    lon = self.request.get("lat")
+    mainHash = {} #main response hash
+    mapHash = {} #map data hash
+    profiles = models.Profile.proximity_fetch(
+          models.Profile.all(), 
+          db.GeoPt(float(lon), float(lat)), 
+          max_results=10)
+    temps = []
+    for p in profiles:
+      plocs = {} #dict for profiles
+      plocs["lon"] = p.location.lat
+      plocs["lat"] = p.location.lon
+      plocs["id"] = p.key().id().__str__()
+      #plocs["picid"] = p.pic.key().id().__str__()
+      #plocs["content"] = p.content
+      plocs["nick"] = p.nick
+      mapHash[p.key().__str__()] = plocs
+      temps.append(plocs)
+      
+    mainHash["mapdata"] = mapHash
+
+    template_values = {
+      "profiles" : temps
+    }
+    path = os.path.join(os.path.dirname(__file__), 'templates/rpc_table.html')
+    templatehtml = template.render(path, template_values)
+    mainHash["tablehtml"] = templatehtml
+    return simplejson.dumps(mainHash)
 		
-		template_values = {
-			"posts" : temps
-		}
-		path = os.path.join(os.path.dirname(__file__), 'templates/rpc_table.html')
-		templatehtml = template.render(path, template_values)
-		mainHash["tablehtml"] = templatehtml
-		return simplejson.dumps(mainHash)
-		
-	def get(self):
-		self.request.path_info_pop()
-		action = self.request.path_info_pop()
-		
-		if (action == 'nearby'):
-			self.response.out.write(self.getNearby())
-			
-		def post(self):
-			self.response.out.write("what?")
+  def get(self):
+    self.request.path_info_pop()
+    action = self.request.path_info_pop()
+    
+    if (action == 'nearby'):
+      self.response.out.write(self.getNearby())
+      
+  def post(self):
+    self.response.out.write("what?")
 
 def main():
   application = webapp.WSGIApplication([('/rpc.*', MainHandler)],
